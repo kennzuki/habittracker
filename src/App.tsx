54c0cd4b3202f3ce0262712f8c2f408cd87feba9
kennzuki <kingsyken@gmail.com>
2024-10-08 02:23:06 +0300

    set up zustand store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
+import useHabitStore from "./store/store"
 
 function App() {
+ const store=useHabitStore()
+ console.log(store);
  
-
   return (
     <>
       <h1 className="text-3xl text-blue-500 font-bold underline">
